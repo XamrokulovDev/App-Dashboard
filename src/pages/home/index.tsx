c98bb99282c6e81950497809a2672f2a8428afdd
ulfatjon <xamrokulovulfatjon2005@gmail.com>
2025-04-03 16:12:25 +0500

    The project is finished

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,11 @@
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="text-2xl text-center my-10">Welcome to Home page</div>
+    <div className="text-2xl text-center my-10">
+      Welcome to Home page
+      <NavLink to="/register" className="bg-blue-500 text-white px-5 py-2 rounded-lg fixed top-10 right-10">Register</NavLink>
+    </div>
   )
 }
 
